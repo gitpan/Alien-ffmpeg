d9c80ef36a517f7bb473d739cc3eb6eee156afef
Torsten Raudssus <torsten@raudssus.de>
2013-02-26 02:40:34 +0000

    Import of GETTY/Alien-ffmpeg-0.002 from CPAN.
    gitpan-cpan-distribution: Alien-ffmpeg
    gitpan-cpan-version:      0.002
    gitpan-cpan-path:         GETTY/Alien-ffmpeg-0.002.tar.gz
    gitpan-cpan-author:       GETTY
    gitpan-cpan-maturity:     released

--- lib/Alien/ffmpeg.pm
+++ lib/Alien/ffmpeg.pm
@@ -1,11 +1,37 @@
 package Alien::ffmpeg;
-
-use strict;
-use warnings;
-
-our $VERSION = 0.001;
-$VERSION = eval $VERSION;
+BEGIN {
+  $Alien::ffmpeg::AUTHORITY = 'cpan:GETTY';
+}
+{
+  $Alien::ffmpeg::VERSION = '0.002';
+}
+# ABSTRACT: Easy installation of ffmpeg
 
 use parent 'Alien::Base';
 
 1;
+
+__END__
+=pod
+
+=head1 NAME
+
+Alien::ffmpeg - Easy installation of ffmpeg
+
+=head1 VERSION
+
+version 0.002
+
+=head1 AUTHOR
+
+Torsten Raudssus <torsten@raudss.us>
+
+=head1 COPYRIGHT AND LICENSE
+
+This software is copyright (c) 2013 by Torsten Raudssus.
+
+This is free software; you can redistribute it and/or modify it under
+the same terms as the Perl 5 programming language system itself.
+
+=cut
+
